39c206a1b739d7e49ed1bb7eb5c08ab968d224ef
Akash Sankritya <sankrityaakash@gmail.com>
2021-04-04 08:39:28 +0530

    Finalised frontend

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,55 +1,56 @@
-import axios from 'axios';
-import authHeader from './auth-header';
-import { config } from '../utils/constants';
+import axios from "axios";
+import authHeader from "./auth-header";
+import { config } from "../utils/constants";
 
-var BASE_API_URL = config.url.API_URL
+var BASE_API_URL = config.url.API_URL;
 
 class AuthService {
+  async login(username, password) {
+    return axios
+      .post(BASE_API_URL + "/login", {
+        password,
+        username,
+      })
+      .then((response) => {
+        sessionStorage.setItem("authToken", response.headers.authorization);
+        console.log(sessionStorage.getItem("authToken"));
+        sessionStorage.setItem("username", username);
+        console.log(sessionStorage.getItem("username"));
+        return response.headers.authorization;
+      });
+  }
 
-    async login(username, password) {
-        return axios
-            .post(BASE_API_URL + '/login', {
-                password,
-                username
-            })
-            .then(response => {
-                sessionStorage.setItem('authToken', response.headers.authorization);
-                console.log(sessionStorage.getItem('authToken'));
-                sessionStorage.setItem('username', username);
-                console.log(sessionStorage.getItem('username'));
-                return response.headers.authorization;
-            });
-    }
+  async register(username, email, password) {
+    return axios.post(BASE_API_URL + "/signup", {
+      username,
+      email,
+      password,
+    });
+  }
 
-    async register(username, email, password) {
-        return axios.post(BASE_API_URL + '/signup', {
-            username,
-            email,
-            password,
-        });
-    }
+  logout() {
+    sessionStorage.removeItem("username");
+    sessionStorage.removeItem("userId");
+    sessionStorage.removeItem("authToken");
+  }
 
-    logout() {
-        sessionStorage.removeItem('username');
-        sessionStorage.removeItem('userId');
-        sessionStorage.removeItem('authToken');
-    }
+  getAuthToken() {
+    return sessionStorage.getItem("authToken");
+  }
 
-    getAuthToken() {
-        return sessionStorage.getItem('authToken');
-    }
+  getCurrentUser() {
+    return sessionStorage.getItem("username");
+  }
 
-    getCurrentUser() {
-        return sessionStorage.getItem('username');
-    }
-
-    async getUserIdByUsername(username) {
-        await axios.get(BASE_API_URL + "/getUserId/" + username, {
-            headers: authHeader()
-        }).then(response => {
-            sessionStorage.setItem('userId', response.data.data);
-            return response.data.data;
-        });
-    }
+  async getUserIdByUsername(username) {
+    await axios
+      .get(BASE_API_URL + "/getUserId/" + username, {
+        headers: authHeader(),
+      })
+      .then((response) => {
+        sessionStorage.setItem("userId", response.data.data);
+        return response.data.data;
+      });
+  }
 }
 export default new AuthService();
